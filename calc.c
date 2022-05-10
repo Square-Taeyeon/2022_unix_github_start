ed7c63cd3e85051753eeba5a928138f67b6c02f7
pnu-cse-edward <pnu.square.edward3@gmail.com>
2022-05-11 03:49:47 +0900

    Correct for PR comments
    - Fix integer overflow problem
    - Improve the testability of the Calc
    - Add test cases for subtraction and addition
    
    Resolves #1

--- calc.c
+++ calc.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
-int add(int a, int b);
-int sub(int a, int b);
+long long add(int a, int b);
+long long sub(int a, int b);
 int main() {
+	char c;
 	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d + %d = %d\n", a, b, add(a, b));
-	printf("%d - %d = %d\n", a, b, sub(a, b));
+	scanf("%c %d %d", &c, &a, &b);
+	long long result = 0;
+	switch(c){
+		case '+': result = add(a, b); break;
+		case '-': result = sub(a, b); break;
+	}
+	printf("%lld\n", result);
 }
 
